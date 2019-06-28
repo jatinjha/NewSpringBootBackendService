feca8fac7d0597a2a12348113a87611a4b4c1255
jpr_007 <jatinjha370@gmail.com>
2019-06-28 20:27:03 +0530

    downoad intializer

---
+++ NewSpringBootBackendService/src/main/java/com/gfg/jatinjha/NewSpringBootBackendService/NewSpringBootBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.jatinjha.NewSpringBootBackendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringBootBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringBootBackendServiceApplication.class, args);
+	}
+
+}
---
+++ NewSpringBootBackendService/src/test/java/com/gfg/jatinjha/NewSpringBootBackendService/NewSpringBootBackendServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gfg.jatinjha.NewSpringBootBackendService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewSpringBootBackendServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
